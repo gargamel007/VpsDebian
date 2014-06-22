e2b26c8fe24b47d33cb66b0aec0cb5f0bf436dfd
gargamel007 <gargamel@m4tp.org>
2014-06-22 14:59:16 +0200

    Removed duplicate entry

--- SetupBase.sh
+++ SetupBase.sh
@@ -45,7 +45,7 @@ echo "UPGRADE  && INSTAL BASE TOOLS"
 apt-get -qq update && apt-get -qq -y upgrade
 INSTPKG="dialog tree vim less screen git htop software-properties-common mosh rsync ncdu"
 #Perl is needed for rename command
-INSTPKG+=" perl sudo locate toilet ufw fail2ban ncdu"
+INSTPKG+=" perl sudo locate toilet ufw fail2ban"
 apt-get install -y -qq $INSTPKG
 
 

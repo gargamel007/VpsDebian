70b2aa8a2a9afdf3e589c8401342dea6c6256759
gargamel007 <gargamel@m4tp.org>
2015-12-05 15:32:22 +0100

    switch to jessie

--- SetupBase.sh
+++ SetupBase.sh
@@ -3,10 +3,10 @@
 ###########################
 #Doc & Usage
 ###########################
-#This script is intended for post install on a Debian 7 system on VPS !
+#This script is intended for post install on a Debian 8 system on VPS !
 :<<'USAGE'
 As root run the follwing commands
-sed -i '/ackster/d' /etc/apt/sources.list && apt-get -qq update && apt-get -y -qq upgrade && apt-get -y -qq install git
+apt-get -qq update && apt-get -y -qq upgrade && apt-get -y -qq install git
 git clone https://github.com/gargamel007/VpsDebian.git Code/VpsDebian
 bash Code/VpsDebian/SetupBase.sh
 USAGE
@@ -16,6 +16,7 @@ USAGE
 ###########################
 BASEDIR=$(dirname $0)
 USERNAME="gargamel"
+
 
 ###########################
 #Main
@@ -36,47 +37,41 @@ if [ ! -f /tmp/is_reset_locale ]; then dpkg-reconfigure locales; fi
 touch /tmp/is_reset_locale
 update-locale
 
-#Fix crappy apt-source list : remove Rackster mirror
-sed -i '/ackster/d' /etc/apt/sources.list
-
-#Add backports mirror
-echo "deb http://ftp.debian.org/debian/ wheezy-backports main contrib non-free" >> /etc/apt/sources.list
-
 #Install Tools
 echo "#############################"
 echo "UPGRADE  && INSTAL BASE TOOLS"
 apt-get -qq update && apt-get -qq -y upgrade
 INSTPKG="dialog tree vim less screen git htop software-properties-common mosh rsync ncdu curl wget"
 #Perl is needed for rename command
-INSTPKG+=" perl sudo locate toilet ufw fail2ban autojump zsh apt-transport-http"
-INSTPKG+=" zsh rubygems-integration"
+INSTPKG+=" perl sudo locate toilet ufw fail2ban autojump zsh"
+INSTPKG+=" tmux"
 
 apt-get install -y -qq $INSTPKG
-#Packages from backports
-apt-get install -y -qq -t wheezy-backports "tmux"
 
-#Install tmuxinator
-gem install tmuxinator
-#Install Oh My Zsh
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-cp -R /root/.oh-my-zsh/ /home/$USERNAME/
-cp /root/.zsh* /home/$USERNAME/
-chown -R $USERNAME:$USERNAME /home/$USERNAME/.*
-local ZSHPATH=`which zsh`
-su $USERNAME -c "chsh -s $ZSHPATH"
+#Packages from backports
+#apt-get install -y -qq -t wheezy-backports "tmux"
+
+#Cleanup
+apt-get -y -qq clean
+
+#Add new user
+adduser $USERNAME
+usermod -s /bin/zsh $USERNAME
+local GROUPS="adm,sudo,users,sshlogin"
+sudo addgroup sshlogin
+usermod -a -G $GROUPS $USERNAME
 
 #Cleanup
 apt-get -y -qq clean
 
 #Secure the ssh server
-sudo addgroup sshlogin
-usermod -a -G sshlogin root
 mv $BASEDIR/FileSystem/etc/issue.net /etc/issue.net
 if [ ! -f /tmp/is_reset_sshd ]; then
  rm /etc/ssh/ssh_host_*
  dpkg-reconfigure openssh-server
 fi
 touch /tmp/is_reset_sshd
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 sed -i "s/X11Forwarding yes/X11Forwarding no/g" /etc/ssh/sshd_config
 sed -i "s/#Banner/Banner/g" /etc/ssh/sshd_config
 sed -i "s/PrintMotd no/PrintMotd yes/g" /etc/ssh/sshd_config
@@ -86,9 +81,6 @@ if ! grep -q AllowGroups /etc/ssh/sshd_config
   fi
 service ssh restart
 sleep 3
-
-#Tweak vim
-sed -i "s/\"syntax on/syntax on/g" /etc/vim/vimrc
 
 #Custom MOTD
 if ! grep -q toilet /etc/init.d/motd
@@ -113,8 +105,8 @@ if `ifconfig|grep -q inet6`
 fi
 sed -i "s/IPV6=yes/IPV6=no/g" /etc/default/ufw
 sed -i "s/^IPT_MODULES/#IPT_MODULES/g" /etc/default/ufw
-sed -i '/#/!{/BROADCAST/{s/^/#/g}}' /etc/ufw/after.rules
-sed -i '/#/!{/ ufw-not-local/{s/^/#/g}}' /etc/ufw/before.rules
+#sed -i '/#/!{/BROADCAST/{s/^/#/g}}' /etc/ufw/after.rules
+#sed -i '/#/!{/ ufw-not-local/{s/^/#/g}}' /etc/ufw/before.rules
 ufw allow ssh
 ufw allow 60000:61000/udp #for mosh
 ufw --force enable
